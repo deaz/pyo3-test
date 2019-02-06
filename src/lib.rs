c05b5781f2bf5720dc462877174e3dde66dc3bd4
Ivan Vologdin <vologdin.nsk@gmail.com>
2019-02-06 15:45:05 +0700

    Add sources

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+struct Class {}
+
+#[pymethods]
+impl Class {
+    #[new]
+    fn __new__(obj: &PyRawObject) -> PyResult<()> {
+        obj.init(|| Class {})
+    }
+}
+
+#[pymodule]
+fn pyo3_test(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Class>()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test() {}
+}
